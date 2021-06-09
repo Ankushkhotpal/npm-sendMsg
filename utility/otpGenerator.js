940603e8501351bde9864147a086e090f7697a4c
Ankush Khotpal <ankush.khotpal@gmail.com>
2021-06-09 14:37:49 +0530

    code formatted

--- utility/otpGenerator.js
+++ utility/otpGenerator.js
@@ -1,24 +1,23 @@
-'strict';
-const rn = require('random-number');
+"strict";
+const rn = require("random-number");
 
- /**
-  * @author Ankush Khotpal
-  * @description Creates a new otp generation instance
-  * @param {integer} to set minimum starting number to generate otp
-  * @param {intiger} to set maximum number
-  */
+/**
+ * @author Ankush Khotpal
+ * @description Creates a new otp generation instance
+ * @param {integer} to set minimum starting number to generate otp
+ * @param {intiger} to set maximum number
+ */
 
 function otp_num(MIN, MAX) {
-    var options = {
-        min: MIN,
-        max: MAX,
-        integer: true
-    }
-    var num = rn(options);
-    return num;
+  var options = {
+    min: MIN,
+    max: MAX,
+    integer: true,
+  };
+  var num = rn(options);
+  return num;
 }
 
-
 module.exports = {
-    otp_num: otp_num,
+  otp_num: otp_num,
 };
